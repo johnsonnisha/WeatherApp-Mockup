567c6be5dee02d2aabc929f311897fe65e69a9a6
nishajohnsongalaxies <waynishajohnson@gmail.com>
2024-04-16 13:16:14 -0700

    Axios and New API elements
    Add axios to html and new elements to start new api function

--- src/app.js
+++ src/app.js
@@ -7,3 +7,7 @@ function showCity(event) {
 }
 let searchElement = document.querySelector("#city-search");
 searchElement.addEventListener("submit", showCity);
+
+letapiKey = "e7bd4to1bb033ed3a0c4b3fbb020c051";
+letapiURL =
+  "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
