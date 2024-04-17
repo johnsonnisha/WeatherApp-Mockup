14c4b63774ec826ba05f4389699805edba6273c8
nishajohnsongalaxies <waynishajohnson@gmail.com>
2024-04-16 18:20:28 -0700

    Checking axios and new functions
    Tested added functions to make sure live data if being retrieved properly. Now able to get live data from city typed in search engine
    functions add: searchCity and updateWeather.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
+function updateWeather(response) {
+  console.log(response.data);
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  letapiKey = "e7bd4to1bb033ed3a0c4b3fbb020c051";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=e7bd4to1bb033ed3a0c4b3fbb020c051&units=metric`;
+  axios.get(apiURL).then(updateWeather);
+  console.log(apiURL);
+}
+
 function showCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-search-input");
   let cityElement = document.querySelector("#current-city");
   let currentCity = searchInput.value;
   cityElement.innerHTML = currentCity;
+  searchCity(searchInput.value);
 }
 let searchElement = document.querySelector("#city-search");
 searchElement.addEventListener("submit", showCity);
-
-letapiKey = "e7bd4to1bb033ed3a0c4b3fbb020c051";
-letapiURL =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
