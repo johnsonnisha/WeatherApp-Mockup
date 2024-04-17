e82e9049107f12c24ef4c30b97b753c3af3fb5da
nishajohnsongalaxies <waynishajohnson@gmail.com>
2024-04-16 21:24:21 -0700

    Time Id and JS Functions
    Add new time section id to start adding current time to the inner HTML. deleted unnecessary console logs and created Date format function, time element and date, time, and days elements. Tested new features.

--- src/app.js
+++ src/app.js
@@ -2,15 +2,38 @@ function updateWeather(response) {
   let currentTemperature = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
   currentTemperature.innerHTML = `${Math.round(temperature)}°C`;
-  console.log(response.data.wind.speed);
-  console.log(response.data.condition.description);
+  console.log(response.data);
+
   let descriptionElement = document.querySelector("#weather-description");
   descriptionElement.innerHTML = response.data.condition.description;
+
   let humidityElement = document.querySelector("#humidity-section");
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
   let windspeedElement = document.querySelector("#windspeed");
   windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let timeElement = document.querySelector("#time-section");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
 }
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let time = `${hours}:${minutes}`;
+  let days = [
+    `Sunday`,
+    `Monday`,
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${time}`;
+}
+
 function searchCity(city) {
   letapiKey = "e7bd4to1bb033ed3a0c4b3fbb020c051";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=e7bd4to1bb033ed3a0c4b3fbb020c051&units=metric`;
